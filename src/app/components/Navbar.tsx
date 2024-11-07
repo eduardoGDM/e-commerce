b4b09139cb4946952aa3ba884e551e675e9f5e9b
EduardoGDM <eduardo.dias@webfloat.com>
2024-11-07 19:36:23 -0300

    teste 2

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,32 +1,66 @@
-import Link from "next/link";
-import {
-  SignIn,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-export default function Navbar() {
-  return (
-    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
-      <Link
-        href="/"
-        className="uppercase font-bold text-md h-12 flex items-center"
-      >
-        Next Store
-      </Link>
-      <div className="flex items-center gap-8">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button className="uppercase rounded-md border-gray-400 px-3 py-4">
-              Fazer Login
-            </button>
-          </SignInButton>
-        </SignedOut>
-      </div>
-    </nav>
-  );
+class DashboardPage extends Component {
+  render() {
+    return (
+      <BetterCircularProgress loading={this.state.loading}>
+        {this.state.data ? (
+          <React.Fragment>
+            <div className="row">
+              <div className="col-sm-12 col-md-12 col-lg-6">
+                <Portlet
+                  className="kt-portlet--border-bottom-brand"
+                  fluidHeight={true}>
+                  <PortletBody>
+                    <div className="kt-widget26">
+                      <div className="kt-widget26__content">
+                        <span className="kt-widget26__number">
+                          {this.state.data.total_usuarios}
+                        </span>
+                        <span className="kt-widget26__desc">Usuários</span>
+                      </div>
+                    </div>
+                  </PortletBody>
+                </Portlet>
+              </div>
+              <div className="col-sm-12 col-md-12 col-lg-6">
+                <Portlet
+                  className="kt-portlet--border-bottom-brand"
+                  fluidHeight={true}>
+                  <PortletBody>
+                    <div className="kt-widget26">
+                      <div className="kt-widget26__content">
+                        <span className="kt-widget26__number">
+                          {this.state.data.total_carros}
+                        </span>
+                        <span className="kt-widget26__desc">
+                          Carros de clientes cadastrados
+                        </span>
+                      </div>
+                    </div>
+                  </PortletBody>
+                </Portlet>
+              </div>
+              <div className="col-sm-12 col-md-12 col-lg-6">
+                <Portlet
+                  className="kt-portlet--border-bottom-brand"
+                  fluidHeight={true}>
+                  <PortletBody>
+                    <div className="kt-widget26">
+                      <div className="kt-widget26__content">
+                        <span className="kt-widget26__number">
+                          {this.state.data.total_anuncios}
+                        </span>
+                        <span className="kt-widget26__desc">
+                          Anúncios de Carregadores cadastrados
+                        </span>
+                      </div>
+                    </div>
+                  </PortletBody>
+                </Portlet>
+              </div>
+            </div>
+          </React.Fragment>
+        ) : null}
+      </BetterCircularProgress>
+    );
+  }
 }
