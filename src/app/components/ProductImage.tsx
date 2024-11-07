49182927083683a0765daf89249d88006edd7eb1
EduardoGDM <eduardo.dias@webfloat.com>
2024-11-07 20:00:08 -0300

    teste pr 3

--- src/app/components/ProductImage.tsx
+++ src/app/components/ProductImage.tsx
@@ -1,25 +1,19 @@
-"use client";
+'use client';
 
-import { ProductType } from "@/types/ProductType";
-import Image from "next/image";
-import { useState } from "react";
+import { ProductType } from '@/types/ProductType';
+import Image from 'next/image';
+import { useState } from 'react';
 type ProductImageProps = {
   product: ProductType;
   fill?: boolean;
 };
-export default function ProductImage({ product, fill }: ProductImageProps) {
-  const [loading, setLoading] = useState(true);
-
+export default function ProductImage({ produc);
+t, fill }: ProductImageProps) {
+  const [loading, setLoading] = useState(true
   return fill ? (
-    <Image
-      src={product.image}
-      fill
+    <Image src={product.image} fill
       alt={product.title}
-      className={`object-cover  ${
-        loading
-          ? "scale-110 blur-3xl grayscale"
-          : "scale-100 blur-0 grayscale-0"
-      }`}
+      className={`object-cover  ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
       onLoad={() => setLoading(false)} //indentificar que a img foi carregado e alterar o state apos isso
     ></Image>
   ) : (
@@ -28,11 +22,7 @@ export default function ProductImage({ product, fill }: ProductImageProps) {
       width={400}
       height={700}
       alt={product.title}
-      className={`object-cover  ${
-        loading
-          ? "scale-110 blur-3xl grayscale"
-          : "scale-100 blur-0 grayscale-0"
-      }`}
+      className={`object-cover  ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
       onLoad={() => setLoading(false)} //indentificar que a img foi carregado e alterar o state apos isso
     ></Image>
   );
